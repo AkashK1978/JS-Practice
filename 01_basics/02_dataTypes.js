4c1f94487d410d58c958f4bd1ef89095b6c09d00
AkashK1978 <160141678+AkashK1978@users.noreply.github.com>
2024-02-17 12:06:35 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,33 @@
+"use strict"; // treat all JS code as newer version..
+
+// alert(3 + 3) // We are using nodejs, not browser
+
+console.log(3
+    + 3) // code readability should be high..
+
+// DATATYPES 
+/*
+1---> number = 2 to the power 53
+2--->bigint = number bada ho toh
+3---> string => ""
+4---> boolean => true or false
+5---> null => standalone value hai
+6---> undefined => var declare kr diya but value define nahi kiya..
+7---> symbol => unique
+
+8---> object
+
+*/
+
+
+
+
+    let name = "Akash"
+    let age = 20
+    let isLoggedIn = false
+    let state = null
+    let city;
+    
+    console.log(typeof age);
+    console.log(typeof null);  // object type hota hai
+    console.log(typeof undefined);  // undefined type hi hota h
