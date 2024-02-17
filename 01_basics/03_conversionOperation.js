18fa43f58b07f2eb2a136849350f90562b9456fc
AkashK1978 <160141678+AkashK1978@users.noreply.github.com>
2024-02-17 12:31:32 +0000

    Conversions

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+// let age = 18
+// let valueInString = String(age)
+// console.log(typeof valueInString);
+
+
+// let score = '33abxy'
+// let valueInNumber = Number (score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); // gives NaN
+
+// let score = null
+// let valueInNumber = Number (score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); // gives 0
+
+
+let score = undefined
+let valueInNumber = Number (score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber); // gives NaN
+
+// "33" => 33
+// "33abdsd" => NaN
+// "true" => 1
+// "false" => 0
+
+let isLoggedIn = 1;
+let valueInBoolean = Boolean(isLoggedIn)
+console.log(typeof valueInBoolean);
+console.log(valueInBoolean)
+
+// 1=>true; 0=> false;
+// "" => false
+// "hitesh" => true 
+
