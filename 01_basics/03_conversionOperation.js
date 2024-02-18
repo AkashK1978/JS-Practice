9fb38214d784aaf22ff08278d30961fdeb1c8032
AkashK1978 <160141678+AkashK1978@users.noreply.github.com>
2024-02-18 13:06:32 +0000

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -14,22 +14,51 @@
 // console.log(valueInNumber); // gives 0
 
 
-let score = undefined
-let valueInNumber = Number (score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber); // gives NaN
+// let score = undefined
+// let valueInNumber = Number (score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); // gives NaN
 
 // "33" => 33
 // "33abdsd" => NaN
 // "true" => 1
 // "false" => 0
 
-let isLoggedIn = 1;
-let valueInBoolean = Boolean(isLoggedIn)
-console.log(typeof valueInBoolean);
-console.log(valueInBoolean)
+// let isLoggedIn = 1;
+// let valueInBoolean = Boolean(isLoggedIn)
+// console.log(typeof valueInBoolean);
+// console.log(valueInBoolean)
 
 // 1=>true; 0=> false;
 // "" => false
 // "hitesh" => true 
 
+// *******************************OPERATIONS*******************************
+
+    let value = 3
+    let negValue = -value
+ //   console.log(negValue);
+    
+    let str1 = "Hello"
+    let str2 = " Akash"
+    let str3 = str1 + str2
+    // console.log(str3);
+
+    // console.log("1" + 2);
+    // console.log(1 + "2");
+    // console.log("1" + 2 + 3); // Agar String Pahle to sabko string me convert kiya jayega
+    // console.log(1 + "2" + 3);
+    // console.log(1 + 2 + "3"); // Agar String last me hai toh pahle sabhi add ho jayenge
+
+    // console.log(+true);
+    // console.log(+"");
+
+    let num1,num2,num3
+     num1 = num2 = num3 = 2+2;
+
+     let gameCounter = 100
+    //  gameCounter++ ------> first returns the value then incrementing.
+     //++gameCounter -------> first incrementing then return Value.
+     console.log(gameCounter);
+     
+     
