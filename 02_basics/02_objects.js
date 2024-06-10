9baac60fa198802bfc25401e5eb1d63aacf0c8bf
AkashK1978 <160141678+AkashK1978@users.noreply.github.com>
2024-06-10 13:08:34 +0000

    objects2

---
+++ 02_basics/02_objects.js
@@ -0,0 +1,85 @@
+// // const tinderUser = new Object()
+// const tinderUser = {}
+
+// tinderUser.id = "123abc",
+// tinderUser.name = "Ak",
+// tinderUser.isLoggedIn = false
+
+// // console.log(tinderUser);
+
+// const regularUser = {
+//     email : "user1@gmail.com",
+//     fullname : {
+//         usersFullName : {
+//             ownName : "Akash Kumawat",
+//             Father_Name : "Neelkamal Kantiwal"
+//         }
+//     }
+// }
+
+// // console.log(regularUser.fullname.usersFullName.Father_Name);
+
+// const  obj1 = {1: "a" , 2 : "b" }
+// const  obj2 = {3: "a" , 4 : "b" }
+
+// // const obj3 = {obj1 , obj2}
+// // const obj3 = Object.assign({},obj1,obj2)
+// const  obj3 = {...obj1, ...obj2}
+// // console.log(obj3);
+
+// const users = [
+//     {
+//         id: 1,
+//         email: "ak@123"
+//     },
+//     {
+//         id: 1,
+//         email: "ak@123"
+//     },
+//     {
+//         id: 1,
+//         email: "ak@123"
+//     },
+//     {
+//         id: 1,
+//         email: "ak@123"
+//     }
+// ]
+
+// users[1].email
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty('isLoggenIn'))
+
+
+// deconstructing
+
+// const course = {
+//     courseName : "JS TUT",
+//     Price : 999,
+//     Instructor : "Chai aur Code"
+// }
+// const{Instructor: inst} = course
+
+// // console.log(Instructor);
+// console.log(inst);
+
+
+// JSON APIs........
+// ....In object format.....
+// {
+//    " name" : "Akash",
+//     "cousename": "JS",
+//    " price" : "free"
+// }
+
+ // ....In array format.....
+ [
+    {},
+    {},
+    {}
+ ]
